c39e261341b147f557361e4c7665ce72e61b1abb
kose yasunori <yasunori04235@gmail.com>
2025-09-08 22:23:56 +0900

    2nd fix cafe.rb

--- cafe.rb
+++ cafe.rb
@@ -20,15 +20,16 @@ def take_order(menus)
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number-1][:name]}(#{menus[order_number-1][:price]}円)ですね。"
-  order_number-1
+  chosen_index = order_number - 1
+  puts "#{menus[chosen_index][:name]}(#{menus[chosen_index][:price]}円)ですね。"
+  chosen_index
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+ordered_drinks = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+ordered_foods = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[ordered_drinks][:price].to_i + FOODS[ordered_foods][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
